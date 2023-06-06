736c48141a0cd5de1e0dd3da95979e06f16b8355
Nick <tortickk8475@gmail.com>
2023-06-06 10:26:54 +0300

    chore: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    VitePWA({
+      mode: "development",
+      base: "/pwa_test/",
+      srcDir: "src",
+      filename: "sw.ts",
+      includeAssets: ["/favicon.png"],
+      strategies: "injectManifest",
+      manifest: {
+        name: "Test Project",
+        short_name: "Test",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "./public/vite.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+          {
+            src: "./public/vite.svg",
+            sizes: "512x512",
+            type: "image/svg",
+          },
+          {
+            src: "./public/vite.svg",
+            sizes: "512x512",
+            type: "image/svg",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: '/pwa_test/',
 })
